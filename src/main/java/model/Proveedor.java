c4c1d71d05306d58d872bffc996a1f9e3cfc979d
miguelbada <miguelenriquebada07@gmail.com>
2019-09-10 19:43:26 -0300

    Verificación de travis

---
+++ src/main/java/model/Proveedor.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Proveedor {
+    private String nombre;
+
+    public Proveedor(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+
+    }
+}
---
+++ src/test/java/model/ProveedorTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProveedorTest {
+
+    @Test
+    public void testVerificacionTravis() {
+        assertEquals(1,1);
+    }
+}
