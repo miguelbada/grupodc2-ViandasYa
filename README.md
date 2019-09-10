e512d9ac70e65f09df20d5e4bcdedf5c3e6631c7
miguelbada <miguelenriquebada07@gmail.com>
2019-09-10 19:22:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# desapp-groupd-backend
---
+++ src/main/java/desappgroupd/grupodc2/Grupodc2Application.java
@@ -0,0 +1,13 @@
+package desappgroupd.grupodc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupodc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupodc2Application.class, args);
+    }
+
+}
